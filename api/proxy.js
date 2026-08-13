7cbda1d4eb18d6b5dfa01b4d2dfcd24cddfa138e
adityabobby2003 <aditya@virtualspheretechnologies.in>
2026-08-13 17:00:08 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   try {
     // Determine backend base URL
-    const BACKEND_API = "http://103.118.16.129:5000" || "http://localhost:5000";
+    const BACKEND_API = "http://169.58.105.116:5000" || "http://localhost:5000";
 
     // Remove /api or /auth prefix from request URL
     const path = req.url.replace(/^\/(api|auth)/, "");
